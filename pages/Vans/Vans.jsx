c861d7dd74e23ebde9c7caac195c2aa858ae3879
Akshay R (A-K-SHA-Y) <akkitricks@gmail.com>
2025-12-25 22:02:55 +0530

    searchParms

--- pages/Vans/Vans.jsx
+++ pages/Vans/Vans.jsx
@@ -1,6 +1,10 @@
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 
 export default function Vans(props) {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const typeFilter = searchParams.get("type");
+  console.log(typeFilter)
+
   let vansData = props.allData !== null ? props.allData.vans : null;
   let vanCards =
     vansData !== null
@@ -28,7 +32,7 @@ export default function Vans(props) {
   return (
     <main className="van-section">
       <h2 className="section-title">Explore our Van Options</h2>
-
+    
       {vansData !== null ? (
         <section className="vans-wrap">{vanCards}</section>
       ) : null}
