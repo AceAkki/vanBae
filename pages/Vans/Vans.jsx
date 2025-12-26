ce37633f20f91d2a55f93839c01a4e9bc99b455a
Akshay R (A-K-SHA-Y) <akkitricks@gmail.com>
2025-12-26 21:02:47 +0530

    filter

--- pages/Vans/Vans.jsx
+++ pages/Vans/Vans.jsx
@@ -3,14 +3,13 @@ import { Link, useSearchParams } from "react-router-dom";
 export default function Vans(props) {
   const [searchParams, setSearchParams] = useSearchParams();
   const typeFilter = searchParams.get("type");
-  console.log(typeFilter)
 
   let vansData = props.allData !== null ? props.allData.vans : null;
   let vanCards =
     vansData !== null
       ? vansData.map((van) => {
           return (
-            <div className="van-card" key={van.id}>
+            <div className={(van.type !== typeFilter) ? "hide" : "van-card"} key={van.id}>
               <Link to={`/vans/${van.id}`}>
                 <div className="van-img">
                   <img src={van.imageUrl} alt="" />
@@ -29,13 +28,26 @@ export default function Vans(props) {
           );
         })
       : null;
+
+  function setFilter(string) {
+    setSearchParams({"type":string})
+  }
+
   return (
     <main className="van-section">
       <h2 className="section-title">Explore our Van Options</h2>
-    
-      {vansData !== null ? (
-        <section className="vans-wrap">{vanCards}</section>
-      ) : null}
+      <div>
+        {vansData !== null ? (
+          <>
+            <div className="filter-wrap">
+              {Array.from(new Set(vansData.map((van) => van.type))).map((dt) => (
+              <button key={dt} onClick={()=> setFilter(dt)}>{dt}</button>
+            ))}
+            </div>
+            <section className="vans-wrap">{vanCards}</section>
+          </>
+        ) : null}
+      </div>
     </main>
   );
 }
