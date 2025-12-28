18768e84dfa2864e3b644a030184334a722195c7
Akshay R (A-K-SHA-Y) <akkitricks@gmail.com>
2025-12-29 00:27:49 +0530

    loader

--- pages/Vans/Vans.jsx
+++ pages/Vans/Vans.jsx
@@ -1,89 +1,100 @@
 import { useEffect, useState } from "react";
-import { Link, useSearchParams } from "react-router-dom";
+import { Link, useSearchParams, useLoaderData } from "react-router-dom";
 
+export function loader() {
+  async function fetchData() {
+    const res = await fetch("/api/vans");
+    if(!res.ok) { throw { message:"Failed to fetch vans!", statusText: res.statusText, status:res.status}}
+    const data = await res.json();
+    return data;
+  }
+  return fetchData();
+
+}
 export default function Vans() {
-  let [vansData, setVansData] = useState(null);
-  const [loading, setLoading] = useState(false);
+  // let [vansData, setVansData] = useState();
+  // const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
   const [searchParams, setSearchParams] = useSearchParams();
   const typeFilter = searchParams.get("type");
+  const vansData = useLoaderData();
 
-
-  useEffect(() => {
-    async function fetchData() {
-      setLoading(true);
-      try {
-        const res = await fetch("/api/vans");
-        if(!res.ok) { throw { message:"Failed to fetch vans!", statusText: res.statusText, status:res.status}}
-        const data = await data.json();
-        setVansData(res);
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     setLoading(true);
+  //     try {
+  //       const res = await fetch("/api/vans");
+  //       if(!res.ok) { throw { message:"Failed to fetch vans!", statusText: res.statusText, status:res.status}}
+  //       const data = await data.json();
+  //       setVansData(res);
         
-      } catch (error) {
-        setError(error)
-      } finally {
-        setLoading(false);
-      }
-    }
-    fetchData();
-  }, []);
+  //     } catch (error) {
+  //       setError(error)
+  //     } finally {
+  //       setLoading(false);
+  //     }
+  //   }
+  //   fetchData();
+  // }, []);
 
- 
-  function VanCards () {
+  function VanCards() {
     return vansData.vans.map((van) => {
-          return (
-            <div
-              className={
-                typeFilter === null || typeFilter === ""
-                  ? "van-card"
-                  : van.type !== typeFilter
-                  ? "hide"
-                  : "van-card"
-              }
-              key={van.id}
-            >
-              <Link to={`${van.id}`} state={{search:`?${searchParams.toString()}`, type:typeFilter}}>
-                <div className="van-img">
-                  <img src={van.imageUrl} alt="" />
-                </div>
-
-                <div className="main-dtl">
-                  <p>{van.name}</p>
-                  <p>{`$${van.price}`}</p>
-                </div>
-                <div className="add-dtl">
-                  <p className={`type-label ${van.type}`}>{van.type}</p>
-                  <p className="light-txt">/day</p>
-                </div>
-              </Link>
+      return (
+        <div
+          className={
+            typeFilter === null || typeFilter === ""
+              ? "van-card"
+              : van.type !== typeFilter
+              ? "hide"
+              : "van-card"
+          }
+          key={van.id}
+        >
+          <Link
+            to={`${van.id}`}
+            state={{ search: `?${searchParams.toString()}`, type: typeFilter }}
+          >
+            <div className="van-img">
+              <img src={van.imageUrl} alt="" />
             </div>
-          );
-        })
-  }
-   
-  function setFilter(key, value) {
-    setSearchParams(oldParam => {
-      value === null ? oldParam.delete(key) : oldParam.set(key, value)
-      return oldParam
+
+            <div className="main-dtl">
+              <p>{van.name}</p>
+              <p>{`$${van.price}`}</p>
+            </div>
+            <div className="add-dtl">
+              <p className={`type-label ${van.type}`}>{van.type}</p>
+              <p className="light-txt">/day</p>
+            </div>
+          </Link>
+        </div>
+      );
     });
   }
 
- 
+  function setFilter(key, value) {
+    setSearchParams((oldParam) => {
+      value === null ? oldParam.delete(key) : oldParam.set(key, value);
+      return oldParam;
+    });
+  }
 
   return (
     <section className="van-section">
       <h2 className="section-title">Explore our Van Options</h2>
       <div className="vans-main-wrap">
-        { (loading) ? <h1>Loading...</h1> : null}
-        { (error) ? <h2 style={{color:"red"}}>{error.message}</h2> : null}
+        {/* {loading ? <h1>Loading...</h1> : null} */}
+        {error ? <h2 style={{ color: "red" }}>{error.message}</h2> : null}
         {vansData !== null ? (
           <>
             <div className="filter-wrap">
               {Array.from(new Set(vansData.vans.map((van) => van.type))).map(
                 (dt) => (
-                  <button 
-                    key={dt} 
+                  <button
+                    key={dt}
                     className={typeFilter === dt ? dt : null}
-                    onClick={() => setFilter("type", dt)}>
+                    onClick={() => setFilter("type", dt)}
+                  >
                     {dt}
                   </button>
                   // <Link key={dt} to={`?type=${dt}`}>
@@ -91,12 +102,9 @@ export default function Vans() {
                   // </Link>
                 )
               )}
-              {
-                typeFilter !== null ?
+              {typeFilter !== null ? (
                 <button onClick={() => setFilter("type", null)}>X</button>
-                : null
-
-              }
+              ) : null}
               {/* <Link to=".">X</Link> */}
             </div>
             <section className="vans-wrap">{<VanCards />}</section>
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
 import { useEffect, useState } from "react";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import {
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
 
 import "./App.css";
 import "./Host.css";
@@ -7,7 +15,7 @@ import "./Host.css";
 import Layout from "../components/Layout.jsx";
 import Home from "../pages/Home.jsx";
 import About from "../pages/About.jsx";
-import Vans from "../pages/Vans/Vans.jsx";
+import Vans, {loader as vansLoader} from "../pages/Vans/Vans.jsx";
 import Van from "../pages/Vans/Van.jsx";
 
 import HostLayout from "../components/HostLayout.jsx";
@@ -24,50 +32,86 @@ import NotFound from "../pages/404.jsx";
 
 import "./server.js";
 
-function App() {
-  return (
-    <>
-      <BrowserRouter>
-        {/* <header>
-          <Link to="/" className="site-logo">
-            #vanBae
-          </Link>
-          <nav>
-            <Link to="/About">About</Link>
-            <Link to="/Vans">Vans</Link>
-          </nav>
-        </header> */}
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="about" element={<About />} />
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route index element={<Home />} />
+      <Route path="about" element={<About />} />
 
-            <Route path="vans" element={<Vans />} />
-            <Route path="vans/:id" element={<Van />} />
+      <Route path="vans" element={<Vans/>} loader={vansLoader}/>
+      <Route path="vans/:id" element={<Van />} />
 
-            {/* no shared element so no needed to wrap it
+      {/* no shared element so no needed to wrap it
              <Route path="vans">
              <Route index element={<Vans allData={vansData}/>} />
              <Route path=":id" element={<Van />} />
              </Route> */}
 
-            <Route path="host" element={<HostLayout />}>
-              <Route index element={<Dashboard />} />
-              <Route path="income" element={<Income />} />
-              <Route path="vans" element={<HostVans />} />
-              <Route path="vans/:id" element={<HostVansDetail />}>
-                <Route index element={<Details />} />
-                <Route path="price" element={<Price />} />
-                <Route path="photos" element={<Photos />} />
-              </Route>
+      <Route path="host" element={<HostLayout />}>
+        <Route index element={<Dashboard />} />
+        <Route path="income" element={<Income />} />
+        <Route path="vans" element={<HostVans />} />
+        <Route path="vans/:id" element={<HostVansDetail />}>
+          <Route index element={<Details />} />
+          <Route path="price" element={<Price />} />
+          <Route path="photos" element={<Photos />} />
+        </Route>
 
-              <Route path="reviews" element={<Reviews />} />
-            </Route>
+        <Route path="reviews" element={<Reviews />} />
+      </Route>
 
-            <Route path="*" element={<NotFound />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <Route path="*" element={<NotFound />} />
+    </Route>
+  )
+);
+
+function App() {
+  return (
+    // <>
+    //   <BrowserRouter>
+    //     {/* <header>
+    //       <Link to="/" className="site-logo">
+    //         #vanBae
+    //       </Link>
+    //       <nav>
+    //         <Link to="/About">About</Link>
+    //         <Link to="/Vans">Vans</Link>
+    //       </nav>
+    //     </header> */}
+    //     <Routes>
+    //       <Route path="/" element={<Layout />}>
+    //         <Route index element={<Home />} />
+    //         <Route path="about" element={<About />} />
+
+    //         <Route path="vans" element={<Vans />} />
+    //         <Route path="vans/:id" element={<Van />} />
+
+    //         {/* no shared element so no needed to wrap it
+    //          <Route path="vans">
+    //          <Route index element={<Vans allData={vansData}/>} />
+    //          <Route path=":id" element={<Van />} />
+    //          </Route> */}
+
+    //         <Route path="host" element={<HostLayout />}>
+    //           <Route index element={<Dashboard />} />
+    //           <Route path="income" element={<Income />} />
+    //           <Route path="vans" element={<HostVans />} />
+    //           <Route path="vans/:id" element={<HostVansDetail />}>
+    //             <Route index element={<Details />} />
+    //             <Route path="price" element={<Price />} />
+    //             <Route path="photos" element={<Photos />} />
+    //           </Route>
+
+    //           <Route path="reviews" element={<Reviews />} />
+    //         </Route>
+
+    //         <Route path="*" element={<NotFound />} />
+    //       </Route>
+    //     </Routes>
+    //   </BrowserRouter>
+    // </>
+    <>
+      <RouterProvider router={router} />
     </>
   );
 }
