b13c7a7d0848332d6c7ddde8347f73504ee0ce9c
Akshay R (A-K-SHA-Y) <akkitricks@gmail.com>
2026-01-03 23:46:27 +0530

    Await and Suspense used

--- pages/Vans/Vans.jsx
+++ pages/Vans/Vans.jsx
@@ -1,15 +1,20 @@
-import { useEffect, useState } from "react";
-import { Link, useSearchParams, useLoaderData } from "react-router-dom";
+import { useEffect, useState, Suspense } from "react";
+import { Link, useSearchParams, useLoaderData, Await } from "react-router-dom";
 
-export function loader() {  
+export function loader() {
   async function fetchData() {
     const res = await fetch("/api/vans");
-    if(!res.ok) { throw { message:"Failed to fetch vans!", statusText: res.statusText, status:res.status}}
+    if (!res.ok) {
+      throw {
+        message: "Failed to fetch vans!",
+        statusText: res.statusText,
+        status: res.status,
+      };
+    }
     const data = await res.json();
     return data;
   }
-  return fetchData();
-
+  return { vansData: fetchData() };
 }
 export default function Vans() {
   // let [vansData, setVansData] = useState();
@@ -17,7 +22,7 @@ export default function Vans() {
   const [error, setError] = useState(null);
   const [searchParams, setSearchParams] = useSearchParams();
   const typeFilter = searchParams.get("type");
-  const vansData = useLoaderData();
+  const {vansData} = useLoaderData();
 
   // useEffect(() => {
   //   async function fetchData() {
@@ -27,7 +32,7 @@ export default function Vans() {
   //       if(!res.ok) { throw { message:"Failed to fetch vans!", statusText: res.statusText, status:res.status}}
   //       const data = await data.json();
   //       setVansData(res);
-        
+
   //     } catch (error) {
   //       setError(error)
   //     } finally {
@@ -37,8 +42,8 @@ export default function Vans() {
   //   fetchData();
   // }, []);
 
-  function VanCards() {
-    return vansData.vans.map((van) => {
+  function VanCards(props) {
+    return props.data.map((van) => {
       return (
         <div
           className={
@@ -85,31 +90,43 @@ export default function Vans() {
       <div className="vans-main-wrap">
         {/* {loading ? <h1>Loading...</h1> : null} */}
         {error ? <h2 style={{ color: "red" }}>{error.message}</h2> : null}
-        {vansData !== null ? (
-          <>
-            <div className="filter-wrap">
-              {Array.from(new Set(vansData.vans.map((van) => van.type))).map(
-                (dt) => (
-                  <button
-                    key={dt}
-                    className={typeFilter === dt ? dt : null}
-                    onClick={() => setFilter("type", dt)}
-                  >
-                    {dt}
-                  </button>
-                  // <Link key={dt} to={`?type=${dt}`}>
-                  //   {dt}
-                  // </Link>
-                )
-              )}
-              {typeFilter !== null ? (
-                <button onClick={() => setFilter("type", null)}>X</button>
-              ) : null}
-              {/* <Link to=".">X</Link> */}
-            </div>
-            <section className="vans-wrap">{<VanCards />}</section>
-          </>
-        ) : null}
+
+
+        {/* without loader had to keep the following as failsafe */}
+        {/* {vansData !== null ? ( */}
+
+        <Suspense fallback={<h1>loading...</h1>}>
+          <Await resolve={vansData}>
+            {(value) => {
+          {console.log(value.vans)}
+              return <>
+                <div className="filter-wrap">
+                  {Array.from(
+                    new Set(value.vans.map((van) => van.type))
+                  ).map((dt) => (
+                    <button
+                      key={dt}
+                      className={typeFilter === dt ? dt : null}
+                      onClick={() => setFilter("type", dt)}
+                    >
+                      {dt}
+                    </button>
+                    // <Link key={dt} to={`?type=${dt}`}>
+                    //   {dt}
+                    // </Link>
+                  ))}
+                  {typeFilter !== null ? (
+                    <button onClick={() => setFilter("type", null)}>X</button>
+                  ) : null}
+                  {/* <Link to=".">X</Link> */}
+                </div>
+                <section className="vans-wrap">{<VanCards data={value.vans} />}</section>
+              </>;
+            }}
+          </Await>
+        </Suspense>
+
+        {/* ) : null} */}
       </div>
     </section>
   );
